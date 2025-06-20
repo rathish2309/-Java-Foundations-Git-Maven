3b510e33535899e923898ca478dfb3bd8e3a7254
Rathish Rajendran <rathish.rajendran.work@gmail.com>
2025-06-20 23:47:38 +0530

    File Handling - Folder and File Operations

---
+++ FileHandling/File/FileHandling_Example.java
@@ -0,0 +1,69 @@
+package FileHandling.File;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class FileHandling_Example {
+
+    //Create File
+    static void createFile(String path) throws IOException {
+        File newFile = new File(path);
+        if (!newFile.exists()) {
+            newFile.createNewFile();
+            System.out.println("New File Created : "+path);
+        } else {
+            System.out.println("File already exists");
+        }
+    }
+
+    static void writeFile(String path) throws IOException {
+        File file = new File(path);
+        if (file.exists()) {
+            FileWriter fileWriter = new FileWriter(path);
+            BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+            bufferedWriter.write("This is a test document");
+            System.out.println("Write Operation is completed");
+            bufferedWriter.close();
+        }
+    }
+
+
+    static void readFile(String path) throws IOException {
+        File file = new File(path);
+        if (file.exists()) {
+            FileReader fileReader = new FileReader(path);
+
+            //to read a data from a file use Scanner Class -> FileWriter Class
+//            Scanner scanner = new Scanner(fileReader);
+//            while (scanner.hasNext()){
+//                //next() -> prints the statement in different lines
+//                //nextLine() -> prints all the statements in a single line
+//                System.out.println(scanner.nextLine());
+//            }
+
+            //BufferedReader Class
+            try (BufferedReader bufferedReader = new BufferedReader(fileReader)) {
+                String line = bufferedReader.readLine();
+                System.out.println("Read File " + line);
+            }
+        }
+    }
+
+    //Delete File
+    static void deleteFail(String path) {
+        File file = new File(path);
+        if(file.exists()){
+            file.delete();
+            System.out.println("File is Deleted");
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        String path = "C:\\Users\\Raphael R Rathish\\Downloads\\Test_Document.txt";
+        createFile(path);
+        writeFile(path);
+        readFile(path);
+        deleteFail(path);
+    }
+
+}
---
+++ FileHandling/Folder/Folder_FileHandling_Example.java
@@ -0,0 +1,44 @@
+package FileHandling.Folder;
+
+import java.io.File;
+
+public class Folder_FileHandling_Example {
+
+//    //create folder
+    static void createFolder(String path) {
+        File newFolder = new File(path);
+        if (!newFolder.exists()) {
+            newFolder.mkdir();
+            System.out.println("New Folder Created");
+        } else {
+            System.out.println("Folder already exists");
+        }
+    }
+
+    //rename a created folder
+    static void renameFolder(String oldPath, String newPath) {
+        File oldFolder = new File(oldPath);
+        File newFolder = new File(newPath);
+        if (oldFolder.exists()) {
+            oldFolder.renameTo(newFolder);
+            System.out.println("Folder name renamed to " + newFolder);
+        }
+    }
+
+    //    delete the created folder
+    static void deleteFolder(String path) {
+        File folder = new File(path);
+        if (!folder.exists()) {
+            folder.delete();
+            System.out.println("Folder is deleted" + path);
+        }
+    }
+
+
+    public static void main(String[] args) {
+        createFolder("C:\\Users\\Raphael R Rathish\\Downloads\\New folder");
+        renameFolder("C:\\Users\\Raphael R Rathish\\Downloads\\New folder",
+                "C:\\Users\\Raphael R Rathish\\Downloads\\New Test Folder");
+        deleteFolder("C:\\Users\\Raphael R Rathish\\Downloads\\New Test Folder");
+    }
+}
